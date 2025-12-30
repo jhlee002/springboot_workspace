8f15d1fbe16ad37970ea977a6b31d237741a3861
이주호 <tiger98210@gmail.com>
2025-12-30 16:49:26 +0900

    Add files via upload
    mallapi 서버

---
+++ mallapi/src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
@@ -0,0 +1,30 @@
+package org.zerock.mallapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.zerock.mallapi.controller.formatter.LocalDateFormatter;
+
+@Configuration
+public class CustomServletConfig implements WebMvcConfigurer{
+
+  //LocalDateFormatter사용
+  @Override
+  public void addFormatters(FormatterRegistry registry) {
+    
+    registry.addFormatter(new LocalDateFormatter());
+  }
+
+  //CORS 설정
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+
+    registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .maxAge(300)
+            .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+  }
+
+}
